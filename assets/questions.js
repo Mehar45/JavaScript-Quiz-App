a508701fbb8f7b7dea124ad91bf6e4af6be51245
Safi <safiu454@gmail.com>
2022-05-24 11:28:43 +0500

    Final commit

---
+++ assets/questions.js
@@ -0,0 +1,45 @@
+const questions = [
+  {
+    questionText: "List the following languages in order of release (oldest to newest)",
+    options: [
+	"1. Java, C++, C#, JavaScript",
+	"2. C++, Java, C#, JavaScript",
+	"3. C++, JavaScript, Java, C#",
+	"4. C++, Java, JavaScript, C#"
+	],
+    answer: "4. C++, Java, JavaScript, C#",
+  },
+  {
+    questionText: "How many bytes is an unsigned short in C?",
+    options: [
+      "1. 8",
+      "2. 1",
+      "3. 4",
+      "4. 2",
+    ],
+    answer: "4. 2",
+  },
+  {
+    questionText:
+      "String values must be enclosed within _____ when being assigned to variables.",
+    options: ["1. commas", "2. curly brackets", "3. quotes", "4. parentheses"],
+    answer: "3. quotes",
+  },
+  {
+    questionText:
+      "Compiler generates ___ file.",
+    options: [
+      "1. Executable code",
+      "2. Object code",
+      "3. Assembly code",
+      "4. None of the above",
+    ],
+    answer: "2. Object code",
+  },
+  {
+    questionText:
+      "Which of the following is a statement that can be used to terminate a loop, switch or label statement?",
+    options: ["1. break", "2. stop", "3. halt", "4. exit"],
+    answer: "1. break",
+  },
+];
